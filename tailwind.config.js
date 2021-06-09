76c6053b8ed61b87345daac277fe687d0ff43e50
mkan0141 <kanzume0141@gmail.com>
2021-06-09 23:28:49 +0900

    version2 feat(tailwind.config.js): add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+    theme: {
+      colors: {
+        black:   '#252525',
+        white:   '#FFF',
+        gray:    '#A5B2BA',
+        puple:   '#CD288A',
+        green:   '#6CD04D',
+        blue:    '#376DC7',
+        skyblue: '#55AFE2',
+      }
+    },
+    variants: {},
+    plugins: []
+  }
